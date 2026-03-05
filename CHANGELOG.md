1456b18b7e1d8f67aaaefdb0f06cbc424c70f4ac
Vonny1412 <yvonne.pautz@outlook.com>
2026-03-05 02:35:56 +0100

    v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,7 @@
 ## v0.3.0
 
 * Added config option "HoldThreshold"
+
+## v1.0.0
+
+* Improved compatibility with other mods by checking `__runOriginal` in the `Tameable.Interact()` patch
--- Patches/Tameable_Interact_Patch.cs
+++ Patches/Tameable_Interact_Patch.cs
@@ -1,21 +1,21 @@
 ﻿using HarmonyLib;
 using HoldToCommand.ValheimAPI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HoldToCommand.Patches
 {
     [HarmonyPatch(typeof(Tameable), "Interact")]
     static class Tameable_Interact_Patch
     {
-        [HarmonyPriority(Priority.First)]
-        static bool Prefix(Tameable __instance, Humanoid user, bool hold, bool alt, ref bool __result)
+        [HarmonyPriority(Priority.Last)]
+        static bool Prefix(Tameable __instance, Humanoid user, bool hold, bool alt, ref bool __result, bool __runOriginal)
         {
+            if (!__runOriginal)
+            {
+                return false;
+            }
             __result = __instance.CustomInteract(user, hold, alt);
-            return false; // we need to block original
+            return false;
         }
     }
 }
